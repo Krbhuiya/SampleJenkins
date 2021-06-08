d6c01202d9771fed1264dc464c6d1dae1a0365b5
Bikramjit <bikramjitbhuiya97@gmail.com>
2021-06-08 10:02:40 +0530

    Second class added

---
+++ src/com/capgemini/sampleJenkins/SecondClass.java
@@ -0,0 +1,10 @@
+package com.capgemini.sampleJenkins;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world on second class.");
+	}
+
+}
